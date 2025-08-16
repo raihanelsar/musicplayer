cc537b49f29f59d853fdfac6a5c91a6d3b9ed78d
raihanelsar <44558995+raihanelsar@users.noreply.github.com>
2025-08-16 08:59:41 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 let progress = document.getElementById("progress");
 let song = document.getElementById("song");
 let ctrlIcon = document.getElementById("ctrlIcon");
+let currentTimeEl = document.getElementById("currentTime");
+let durationEl = document.getElementById("duration");
 
 song.onloadedmetadata = function(){
   progress.max = song.duration;
-  progress.value = song.currentTime;
+  durationEl.innerHTML = formatTime(song.duration);
 }
 
 function playPause(){
@@ -12,23 +14,25 @@ function playPause(){
     song.pause();
     ctrlIcon.classList.remove("fa-pause");
     ctrlIcon.classList.add("fa-play");
-  }
-  else{
+  } else {
     song.play();
-    ctrlIcon.classList.add("fa-pause");
     ctrlIcon.classList.remove("fa-play");
+    ctrlIcon.classList.add("fa-pause");
   }
 }
-if(song.play()){
-setInterval(()=>{
-progress.value = song.currentTime;
-},500);
 
+song.ontimeupdate = function(){
+  progress.value = song.currentTime;
+  currentTimeEl.innerHTML = formatTime(song.currentTime);
 }
 
-progress.onchange = function(){
-song.play();
-song.currentTime = progress.value;
-ctrlIcon.classList.add("fa-pause");
-ctrlIcon.classList.remove("fa-play");
+progress.oninput = function(){
+  song.currentTime = progress.value;
+}
+
+function formatTime(seconds){
+  let min = Math.floor(seconds / 60);
+  let sec = Math.floor(seconds % 60);
+  if(sec < 10){ sec = "0" + sec }
+  return `${min}:${sec}`;
 }
